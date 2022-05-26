370bae3e3c25af396f5ad9a828004f5a025e1f83
Ben Newman <benewman2002@gmail.com>
2022-05-26 20:52:31 +0100

    wrote tests for single line db queries

---
+++ backend/__tests__/database.spec.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import pool from "../src/database";
+
+describe("database queries", () => {
+	it("returns 10 when queries to 'SELECT 10;'", (done) => {
+		pool.query("SELECT 10;", (err, res) => {
+			if (err) return done(err);
+			try {
+				expect(res).toEqual([{ "10": 10 }]);
+				done();
+			} catch (err) {
+				done(err);
+			}
+		});
+	});
+
+	it("returns the correct observation type given an observation ID", (done) => {
+		pool.query("SELECT event_type FROM events WHERE id = '01f547a3-7eb1-48f0-86d2-ddc996abd617';", (err, res) => {
+			if (err) return done(err);
+			try {
+				expect(res).toEqual([{ event_type: "mood_observation" }]);
+				done();
+			} catch (err) {
+				done(err);
+			}
+		});
+	});
+});
