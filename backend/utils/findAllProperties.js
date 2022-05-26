fe283ed03d95327577f0b37691530f56d521e3e4
Ben Newman <benewman2002@gmail.com>
2022-05-26 22:36:05 +0100

    wrote utilities to get database metadata

---
+++ backend/utils/findAllProperties.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const fs = require("fs");
+const { createPool } = require("mysql");
+
+const pool = createPool({
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PWORD,
+	port: process.env.DB_PORT,
+});
+
+pool.getConnection(async (err, conn) => {
+	if (err) throw err;
+	conn.query("SELECT payload FROM events;", (err, res) => {
+		let allKeys = new Set();
+		if (err) throw err;
+		// console.log(res[0]);
+		res.forEach((element, i) => {
+			if (i % 100) console.log(i + "/" + res.length);
+			let keys = Object.keys(JSON.parse(element["payload"]));
+			keys.forEach((key) => {
+				allKeys.add(key);
+			});
+		});
+		fs.writeFile("allprops.json", JSON.stringify(Array.from(allKeys)), (err) => {
+			if (err) throw err;
+			console.log("Done!");
+		});
+	});
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import "./App";
+
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className="container">
+			<header className="w3-container w3-indigo">
 				<h1>Hello World!</h1>
 			</header>
-			<nav>
+			<nav className="w3-dropdown-hover">
+				<button className="w3-button"></button>
 				<Link to="/event-type">View by event type</Link>
 			</nav>
 		</div>
