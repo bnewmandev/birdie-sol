b45765e022a5e9a959f66fab71f4b016474999a1
Ben Newman <benewman2002@gmail.com>
2022-06-03 17:11:18 +0100

    added cors headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,12 @@ dotenv.config();
 
 const app = express();
 
+app.use((_, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(pingController);
 app.use(validateUserController);
 app.use(fetchUserController);
