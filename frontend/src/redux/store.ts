1a874ef11dcbd803b6397eb8ccd2e338d18b57ae
Ben Newman <ben@bndev.co.uk>
2022-06-03 10:34:40 +0100

    events redux implementation

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import eventsSlice from "./events";
 
-export default configureStore({
-	reducer: {},
+const store = configureStore({
+	reducer: { events: eventsSlice },
 });
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
